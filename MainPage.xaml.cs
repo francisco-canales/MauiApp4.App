917405b793b89f2b8488598e9c73c15274f24d1e
canalesf23 <canalesf23@gmail.com>
2023-06-08 09:33:41 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnCambio_Clicked(object sender, EventArgs e)
+    {
+        double Pago = Convert.ToDouble(pago.Text);
+        double Costo = Convert.ToDouble(costo.Text);
+        if (Pago>Costo)
+        {
+
+            double calculo = Pago - Costo;
+
+            DisplayAlert("Cambio Calculado", Convert.ToString(calculo).PadLeft(2, '0'), "Aceptar");
+        }
+        else
+        {
+            DisplayAlert("Error", "NO se pude calcular", "ok");
+        }
+
+
+    }
+}
+
